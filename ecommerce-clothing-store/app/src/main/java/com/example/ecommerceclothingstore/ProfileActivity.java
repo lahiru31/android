9165a9bf5c1978a397e570e7b8b79a719d1808be
CodeSandbox <support@codesandbox.io>
2025-02-10 11:08:12 +0000

    Fixed bugs, added Google Maps to Profile Screen

--- ecommerce-clothing-store/app/src/main/java/com/example/ecommerceclothingstore/ProfileActivity.java
+++ ecommerce-clothing-store/app/src/main/java/com/example/ecommerceclothingstore/ProfileActivity.java
@@ -2,11 +2,49 @@ package com.example.ecommerceclothingstore;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.GoogleMap;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Set up the map here
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
